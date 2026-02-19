080f3ba1c62670c7e6692707911a653d0d3ef002
khyati bhardwaj <khyatibhardwaj0103@gmail.com>
2026-02-19 16:54:56 +0530

    Implement palindrome check in DAY10.C
    Fix typo in palindrome check and add missing header.

---
+++ DAY10.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    char str[1000];
+    scanf("%s",str);
+    int left = 0;
+    int right = strlen(str) - 1;
+    bool isPalindrome = true;
+
+    while (left < right){
+          if (str[left] != str[right]){
+              isPalindeome = false;
+              break;
+          }
+          left++;
+          right--;
+    }
+    printf("%s\n",isPalindrome ? "YES" : "NO");
+    return 0 ;
+}
