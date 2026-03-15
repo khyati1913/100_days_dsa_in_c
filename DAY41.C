29119ed21bd3b8c25e3b3e98dbb70d94b6436c0a
khyati bhardwaj <khyatibhardwaj0103@gmail.com>
2026-03-15 11:32:36 +0530

    Implement queue data structure in DAY41.C
    Fix memory allocation and syntax errors in queue implementation.

---
+++ DAY41.C
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+#inslude
+/* Node Structure */
+typedef struct Node {
+    int data;
+    struct Node* next;
+}Node;
+/* Queue Structure */
+typedef  struct myQueue {
+    int currSize;
+    Node* front;
+    Node* rear;
+} myQueue;
+
+Node* newNode(int data){
+    Node* node  = (Node*)malloc(sizeof(Node));
+    if (node == NULL){
+      printf("Memory allocation failed \n");
+      exit(1);
+    }
+    node->data = data;
+    node->bext = NULL;
+    return node;
+}
+myQueue* createQueue(){
+      myQueue* q = (myQueue*)malloc(sizeof(myQueue));
+      if ( q == NULL){
+          printf("Memory allocation failed\n");
+          exit(1);
+      }
+      q->front = NULL;
+      q->rear = NULL;
+      q->currSize = 0;
+      return q;
+}
+int isEmpty(myQueue* q) {
+    return q->front == NULL;
+}
+void enqueue(myQueue* q , int data){
+    Node* node = newNode(data);
+    if (isEmpty(q)) {
+        q->front = q->rear = node;
+    } else {
+        q->rear->next = node;
+        q->rear = node;
+  }
+  q->currSize++;
+}
+int dequeue(myQueue* q) {
+    if (isEmpty(q)) {
+        return -1;
+    }
+    Node* temp = q->front;
+    int rempvedData = temp->data;
+    q->front = q->front->next;
+    if (q->front == NULL)
+        q->rear = NULL;
+    free(temp);
+    q->currSize--;
+    return removedData;
+}
+
+
+
+
