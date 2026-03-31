043a9ce55381930deb2bcce985acc8631f0068fa
khyati bhardwaj <khyatibhardwaj0103@gmail.com>
2026-03-31 20:13:24 +0530

    Implement level order traversal of binary tree

---
+++ DAY54.C
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 1000
+struct Node {
+    int data;
+    struct Node *left,*right;
+};
+struct Node* createNode(int data) {
+    if (data == -1) return NULL;
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = data;
+    n->left = n->right = NULL;
+    return n;
+}
+// build tree from level order
+struct Node* buildTree(int arr[],int n, int i) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+    struct Node* root = createNode(arr[i]);
+    root->left = buildTree(arr,n,2*i+1);
+    root->right = buildTree(arr,n,2*i+2);
+    return root;
+}
+int main() {
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    for (int i = 0; i < N;i++)
+        scanf("%d",&arr[i]);
+    struct Node* root = buildTree(arr,N,0)
+    if (root == NULL) return 0;
+    struct Node* queue[MAX];
+    int front = 0,rear = 0;
+    queue[rear++] = root;
+    int leftToRight = 1;
+    while (front < rear) {
+        int levelSize = rear - front;
+        int level[levelSize];
+        for (int i = 0 ; i < levelSize;i++){
+            struct Node* curr = queue[front++];
+            level[i] = curr->data;
+            if (curr->left)
+              queue[rear++] = curr->left;
+            if (curr->right)
+              queue[rear++] = curr->right;
+        }
+        if (leftToRight) {
+            for (int i = 0 ; i < levelsize ;i++)
+                printf("%d",level[i]);
+        } else {
+            for (int i = levelSize - 1 ; i >= 0 ; i--)
+                printf("%d",level[i]);
+        }
+        leftToRight = !leftToRight;
+    }
+    return 0;
+}
