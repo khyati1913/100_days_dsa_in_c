4b2bc4124570ef35c850da46d48013d4dce75984
khyati bhardwaj <khyatibhardwaj0103@gmail.com>
2026-05-04 09:42:37 +0530

    Add bubble sort implementation in DAY81.c
    Implement bubble sort to sort an array of integers.

---
+++ DAY81.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[1000];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    // Bubble Sort
+    for (int i = 0; i < n - 1; i++) {
+        int swapped = 0;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        if (!swapped) break;  // already sorted
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
