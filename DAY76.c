25620775c7c6690bddf35bff6e7ca77b1058aee7
khyati bhardwaj <khyatibhardwaj0103@gmail.com>
2026-05-04 09:37:11 +0530

    Implement DFS for counting connected components

---
+++ DAY76.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int n, m;
+int adj[MAX][MAX];
+int visited[MAX];
+
+void dfs(int v) {
+    visited[v] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[v][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    // Initialize adjacency matrix and visited
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;  // undirected graph
+    }
+
+    int components = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
